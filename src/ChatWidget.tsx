84a74ea78c0d0d13a7ca9e5f27f89e84bbcfbe06
Agricog <mick@mickstevenson.com>
2026-02-05 06:54:37 +0000

    Update ChatWidget.tsx

--- src/ChatWidget.tsx
+++ src/ChatWidget.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, } from 'react';
 import { Send, X, MessageCircle } from 'lucide-react';
 import { useTranslation } from './useTranslation';
 
